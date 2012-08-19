7fe54e5cc74fc91d3edf8f028a06eb5948802e25
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-08-19 22:23:13 +0100

    Make compile live mode work with index.html files. Addresses issue #10.

--- live.js
+++ live.js
@@ -1,112 +1,122 @@
-// file and dir path of currently-viewed HTML page
-var currentFilePath = location.pathname;
-var currentDirPath = location.pathname.substr(0,
-  location.pathname.lastIndexOf('/'));
+(function() {
+  // file and dir path of currently-viewed HTML page
+  var currentFilePath = location.pathname;
 
-// socket communication
-var socket = io.connect('http://localhost:' + location.port);
+  // paths ending in / are directories that represent index.html
+  if (currentFilePath.charAt(currentFilePath.length - 1) === '/') {
+    currentFilePath += 'index.html';
+  }
 
-// link tag information
-var linkTags = document.querySelectorAll('link');
-var linkTag;
-var linkHref;
+  var currentDirPath = location.pathname.substr(0,
+    location.pathname.lastIndexOf('/'));
 
-// map of absolute source path => [link tag, original source path]
-var linkSources = {};
-var linkSourceList = [];
+  // socket communication
+  var socket = io.connect('http://localhost:' + location.port);
 
-// script tag information
-var scriptTags = document.querySelectorAll('script');
-var scriptTag;
-var scriptSrc;
+  // link tag information
+  var linkTags = document.querySelectorAll('link');
+  var linkTag;
+  var linkHref;
 
-// map of absolute source path => [script tag, original source path]
-var scriptSources = {};
-var scriptSourceList = [];
+  // map of absolute source path => [link tag, original source path]
+  var linkSources = {};
+  var linkSourceList = [];
 
-// record sources of all JS files
-for (i = 0; i < scriptTags.length; i++) {
-  scriptTag = scriptTags[i];
-  scriptSrc = scriptTag.getAttribute('src');
+  // script tag information
+  var scriptTags = document.querySelectorAll('script');
+  var scriptTag;
+  var scriptSrc;
 
-  // record only if src exists
-  if (scriptSrc) {
-    scriptSrc = scriptSrc.split('?')[0];
-    origScriptSrc = scriptSrc;
+  // map of absolute source path => [script tag, original source path]
+  var scriptSources = {};
+  var scriptSourceList = [];
 
-    // make path relative to root
-    if (scriptSrc[0] !== '/') {
-      scriptSrc = currentDirPath + '/' + scriptSrc;
+  var i;
+
+  // record sources of all JS files
+  for (i = 0; i < scriptTags.length; i++) {
+    scriptTag = scriptTags[i];
+    scriptSrc = scriptTag.getAttribute('src');
+
+    // record only if src exists
+    if (scriptSrc) {
+      scriptSrc = scriptSrc.split('?')[0];
+      origScriptSrc = scriptSrc;
+
+      // make path relative to root
+      if (scriptSrc[0] !== '/') {
+        scriptSrc = currentDirPath + '/' + scriptSrc;
+      }
+      scriptSources[scriptSrc] = [scriptTag, origScriptSrc];
     }
-    scriptSources[scriptSrc] = [scriptTag, origScriptSrc];
   }
-}
 
-// record sources of all CSS files
-for (i = 0; i < linkTags.length; i++) {
-  linkTag = linkTags[i];
-  linkHref = linkTag.getAttribute('href');
+  // record sources of all CSS files
+  for (i = 0; i < linkTags.length; i++) {
+    linkTag = linkTags[i];
+    linkHref = linkTag.getAttribute('href');
 
-  // record only if href exists
-  if (linkHref) {
-    linkHref = linkHref.split('?')[0];
-    origLinkHref = linkHref;
+    // record only if href exists
+    if (linkHref) {
+      linkHref = linkHref.split('?')[0];
+      origLinkHref = linkHref;
 
-    // make path relative to root
-    if (linkHref[0] !== '/') {
-      linkHref = currentDirPath + '/' + linkHref;
+      // make path relative to root
+      if (linkHref[0] !== '/') {
+        linkHref = currentDirPath + '/' + linkHref;
+      }
+      linkSources[linkHref] = [linkTag, origLinkHref];
     }
-    linkSources[linkHref] = [linkTag, origLinkHref];
-  }
-}
-
-// extract keys to get a list of link/script sources
-for (var linkSource in linkSources) {
-  linkSourceList.push(linkSource);
-}
-
-for (var scriptSource in scriptSources) {
-  scriptSourceList.push(scriptSource);
-}
-
-// paths need to be normalized; have the server make them absolute
-socket.emit('resolvePaths', [currentFilePath, linkSourceList,
-  scriptSourceList]);
-
-socket.on('pathsResolved', function(paths) {
-  // paths is now an array of absolute file paths
-  var origSource;
-  currentFilePath = paths[0];
-
-  // paths[1] and paths[2] are objects of the form:
-  // {
-  //  absolute path 1: original path 1,
-  //  absolute path 2: original path 2,
-  //  ...
-  // }
-  for (var linkSource in paths[1]) {
-    origSource = paths[1][linkSource];
-    linkSources[linkSource] = linkSources[origSource];
-    delete linkSources[origSource];
   }
 
-  for (var scriptSource in paths[2]) {
-    origSource = paths[2][scriptSource];
-    scriptSources[scriptSource] = scriptSources[origSource];
-    delete scriptSources[origSource];
+  // extract keys to get a list of link/script sources
+  for (var linkSource in linkSources) {
+    linkSourceList.push(linkSource);
   }
 
-  console.log(currentFilePath, linkSources, scriptSources);
-});
-
-socket.on('fileModified', function(filePath) {
-  if (filePath in linkSources) {
-    linkTag = linkSources[filePath][0];
-    // add cache-busting query string to force the browser to reload this css
-    linkTag.setAttribute('href', linkSources[filePath][1] + '?' +
-      new Date().getTime());
-  } else if (filePath in scriptSources || filePath == currentFilePath) {
-    // true parameter forces browser to skip cache
-    location.reload(true);
+  for (var scriptSource in scriptSources) {
+    scriptSourceList.push(scriptSource);
   }
-});
+
+  // paths need to be normalized; have the server make them absolute
+  socket.emit('resolvePaths', [currentFilePath, linkSourceList,
+    scriptSourceList]);
+
+  socket.on('pathsResolved', function(paths) {
+    // paths is now an array of absolute file paths
+    var origSource;
+    currentFilePath = paths[0];
+
+    // paths[1] and paths[2] are objects of the form:
+    // {
+    //  absolute path 1: original path 1,
+    //  absolute path 2: original path 2,
+    //  ...
+    // }
+    for (var linkSource in paths[1]) {
+      origSource = paths[1][linkSource];
+      linkSources[linkSource] = linkSources[origSource];
+      delete linkSources[origSource];
+    }
+
+    for (var scriptSource in paths[2]) {
+      origSource = paths[2][scriptSource];
+      scriptSources[scriptSource] = scriptSources[origSource];
+      delete scriptSources[origSource];
+    }
+
+    console.log(currentFilePath, linkSources, scriptSources);
+  });
+
+  socket.on('fileModified', function(filePath) {
+    if (filePath in linkSources) {
+      linkTag = linkSources[filePath][0];
+      // add cache-busting query string to force the browser to reload this css
+      linkTag.setAttribute('href', linkSources[filePath][1] + '?' +
+        new Date().getTime());
+    } else if (filePath in scriptSources || filePath == currentFilePath) {
+      // true parameter forces browser to skip cache
+      location.reload(true);
+    }
+  });
+}());
